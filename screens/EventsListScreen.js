c67e7ac1d14a80c95b2f14361e7aab003adcf403
SStubbsy <jacob.stubbs1@studytafensw.edu.au>
2026-05-26 12:32:11 +1000

    feat: adding api layer service

--- screens/EventsListScreen.js
+++ screens/EventsListScreen.js
@@ -1,8 +1,44 @@
-import { StyleSheet, View } from 'react-native'
-import { Text, Button } from 'react-native-paper'
-import React from 'react'
+import { StyleSheet, View, FlatList, ActivityIndicator } from 'react-native'
+import { Text, Button, Card } from 'react-native-paper'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { getAllEvents } from '../services/apiService';
 
 const EventsListsScreen = (props) => {
+    const [events, setEvents] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [offline, setOffline] = useState(false);
+    const [error, setError] = useState([]);
+
+    useEffect(() => {
+        console.log("Hello from Jacob")
+        loadEvents();
+    }, []);
+
+    async function loadEvents() {
+        console.log("LoadEvent")
+        setLoading(true);
+        const result = await getAllEvents();
+        if (result.success) {
+            setEvents(result.events);
+            setOffline(result.offline);
+            console.log(events)
+        }
+        else {
+            setError(result.error)
+        }
+        setLoading(false)
+    }
+
+    if (loading) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <ActivityIndicator size="large" />
+                <Text>"Loading Events..."</Text>
+            </View>
+        )
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>EventsListScreen</Text>
@@ -15,7 +51,35 @@ const EventsListsScreen = (props) => {
             }}>
                 Press me
             </Button>
-        </View>
+            {offline && (
+                <View>
+                    <Text>
+                        Text offline mode
+                    </Text>
+                </View>
+            )}
+            <FlatList
+                data={events}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <View style={styles.card} >
+                        <Text sytle={styles.title}>
+                            {item?.title}
+                        </Text>
+                        <Text>
+                            {item?.date}
+                        </Text>
+                        <Text>
+                            {item?.location}
+                        </Text>
+                        <Text>
+                            {item?.spotRemaining}
+                        </Text>
+                    </View>
+                )
+                }
+            />
+        </View >
     )
 }
 
@@ -24,11 +88,17 @@ export default EventsListsScreen
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fa7874',
-        justifyContent: 'center',
+        padding: 12,
     },
     title: {
-        textAlign: 'center',
-        fontWeight: 'bold'
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    card: {
+        padding: 16,
+        marginBottom: 12,
+        borderWidth: 1,
+        borderColor: "#3d3d3d",
+        borderRadius: 8,
     }
 })
